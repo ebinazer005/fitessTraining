3328540e9d59f0ba9afe2506d5badc53a6ea7908
ebinazer005 <ebinazerselvaraj206@gmail.com>
2025-11-17 23:16:42 +0530

    front end style

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -5,24 +5,43 @@ import "./home.css"
 const Home = () => {
     return ( <>
     <Navbar />
-    <div className="home">
+     <h1 className="home_title"><span>The Fitness </span> Journey <br /> Start here</h1>
+     <div className="home"> */
 
 
-      <div className="home_card">    
-         <h1 className="home_title"><span>The Fitness </span> Journey <br /> Start here</h1>
+      
 
             <div className="flex-cards">
-               <div className="first-cart"><h1>kj</h1></div>
-               <div className="sec-cart"><h1>kj</h1></div>
+               <div>
+                  <h2 className="card-title"><span>UNLEASH YOUR</span> POTANTIAL</h2>
+               <p className="card-desc">see real program with expert guidence and Ebinazer Training methods Get Started and unloack a through healthier you</p>
+               </div>
+               
+               <div className="first-cart">
+                  <img src="/public/home/pexels-panther-1547248.jpg"/>
+                  
+               </div>
+               <div className="sec-cart">
+                  <img src="/public/home/pexels-pixabay-416778.jpg"/>
+                 
+               </div>
+               <div className="sec-card-title">
+                  <h3 >BUILD YOUR MUSCLE</h3>
+                  <h3 >BUILD YOUR MUSCLE</h3>
+                  <h3 >BUILD YOUR MUSCLE</h3>
+               
+               </div>
+              
             </div>
          
-         <div className="assistant_wrapper">
-            <Assistent />
-         </div>  
+           
          
-      </div>
-       
-    </div>  
+      {/* </div> */}
+       <div className="assistant_wrapper">
+            <Assistent />
+         </div>
+     </div>   
+     
  </> );
 }
  
